1e54a171cf75891836958c04dfc8885f1e315038
Kyle Morgan <kmorgan8588@gmail.com>
2019-11-29 12:51:48 -0800

    adds mongoose uri port as requested from review

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/listings';
+const URI = 'mongodb://localhost:27017/listings';
 const db = mongoose.connect(URI, {useNewUrlParser: true,  useUnifiedTopology: true });
 
 
