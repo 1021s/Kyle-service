d71836dbd21484a23b2bbb1e06e4b756ca44b5b2
Kyle Morgan <kmorgan8588@gmail.com>
2019-11-29 13:43:51 -0800

    sets up most of the seeding script necessary for seeding db

--- seed/index.js
+++ seed/index.js
@@ -2,24 +2,40 @@
 
 
 //loop 100 times
-for(let i = 0; i < 100; i++) {
+const houses = [];
+for (let i = 0; i < 100; i++) {
     const house = {};
-    //generate random price, 6 - 7 digits
-    //details
-    //random room count 2-8
-    //random bathroom count: 1-4
-    //random sq footage: look up reasonable sq footage
-    //address {}
-    //random house number 4 digits
-    //random street number 1 - 3 digits
-    //random street: either st or ave w NE, NW, SE, S, N as addition tackons
-    //seattle
-    //wa
-    //random zip
-    //zestimate flip between true or false
-    //agents flip between true or false
-    //saved defaults to false
+
+    house['Price'] = Math.floor(Math.random() * 5999999) + 100000;
+
+
+    const Detail = {};
+    Detail['Room_count'] = Math.floor(Math.random() * 6) + 2;
+
+    Detail['Bathroom_count'] = Math.floor(Math.random() * 3) + 1;
+
+    Detail['Square_footage'] = Math.floor(Math.random() * 2600) + 500;;
+    house['Details'] = Detail;
+
+    const Address = {};
+    Address['House_number'] = Math.floor(Math.random() * 9888) + 111;
+    Address['Street_number'] = Math.floor(Math.random() * 300);
+    const street = ['street', 'avenue', 'place', 'boulevard'];
+    const direction = ['', ' NW', ' NE', ' S', ' SW', ' SE', ' N'];
+    Address['Street'] = street[Math.floor(Math.random() * 4)] + direction[Math.floor(Math.random() * 7)];
+    Address['City'] = 'Seattle';
+    Address['State'] = 'WA';
+    Address['ZIP_code'] = Math.floor(Math.random() * 99) + 98100;
+    house['Address'] = Address;
+
+
+    const bools = [true, false];
+    house['Zestimate'] = bools[Math.floor(Math.random() * 2)];
+    house['Agent'] = bools[Math.floor(Math.random() * 2)];
     house['Saved'] = false;
-     House.create(house);//create and save record in db
-    
-    }
+
+    houses.push(house);
+  
+    // House.create(house);//create and save record in db
+
+}
