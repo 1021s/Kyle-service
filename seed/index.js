b753fed1d2b4f8d32e813ecc44d3589f1ced94d5
Kyle Morgan <kmorgan8588@gmail.com>
2019-11-29 13:57:48 -0800

    adds the listing id key to the house being created, also creates a basic test for the script

--- seed/index.js
+++ seed/index.js
@@ -2,40 +2,54 @@
 
 
 //loop 100 times
-const houses = [];
-for (let i = 0; i < 100; i++) {
-    const house = {};
+const seed = () => {
+    const houses = [];
+    for (let i = 0; i < 100; i++) {
+        const house = {};
+        let pad = i;
 
-    house['Price'] = Math.floor(Math.random() * 5999999) + 100000;
+        if (pad.toString().length < 2) {
+            house['Listing_id'] = '00' + pad.toString();
+        } else {
+            house['Listing_id'] = '0' + pad.toString();
+        }
+
+        house['Price'] = Math.floor(Math.random() * 5999999) + 100000;
+
+        const Detail = {};
+
+        Detail['Room_count'] = Math.floor(Math.random() * 6) + 2;
+        Detail['Bathroom_count'] = Math.floor(Math.random() * 3) + 1;
+        Detail['Square_footage'] = Math.floor(Math.random() * 2600) + 500;;
+
+        house['Details'] = Detail;
+
+        const Address = {};
+        Address['House_number'] = Math.floor(Math.random() * 9888) + 111;
+        Address['Street_number'] = Math.floor(Math.random() * 300);
+
+        const street = ['street', 'avenue', 'place', 'boulevard'];
+        const direction = ['', ' NW', ' NE', ' S', ' SW', ' SE', ' N'];
+
+        Address['Street'] = street[Math.floor(Math.random() * 4)] + direction[Math.floor(Math.random() * 7)];
+        Address['City'] = 'Seattle';
+        Address['State'] = 'WA';
+        Address['ZIP_code'] = Math.floor(Math.random() * 99) + 98100;
+        house['Address'] = Address;
 
 
-    const Detail = {};
-    Detail['Room_count'] = Math.floor(Math.random() * 6) + 2;
+        const bools = [true, false];
 
-    Detail['Bathroom_count'] = Math.floor(Math.random() * 3) + 1;
+        house['Zestimate'] = bools[Math.floor(Math.random() * 2)];
+        house['Agent'] = bools[Math.floor(Math.random() * 2)];
+        house['Saved'] = false;
 
-    Detail['Square_footage'] = Math.floor(Math.random() * 2600) + 500;;
-    house['Details'] = Detail;
+        houses.push(house);
 
-    const Address = {};
-    Address['House_number'] = Math.floor(Math.random() * 9888) + 111;
-    Address['Street_number'] = Math.floor(Math.random() * 300);
-    const street = ['street', 'avenue', 'place', 'boulevard'];
-    const direction = ['', ' NW', ' NE', ' S', ' SW', ' SE', ' N'];
-    Address['Street'] = street[Math.floor(Math.random() * 4)] + direction[Math.floor(Math.random() * 7)];
-    Address['City'] = 'Seattle';
-    Address['State'] = 'WA';
-    Address['ZIP_code'] = Math.floor(Math.random() * 99) + 98100;
-    house['Address'] = Address;
+        // House.create(house);//create and save record in db
 
-
-    const bools = [true, false];
-    house['Zestimate'] = bools[Math.floor(Math.random() * 2)];
-    house['Agent'] = bools[Math.floor(Math.random() * 2)];
-    house['Saved'] = false;
-
-    houses.push(house);
-  
-    // House.create(house);//create and save record in db
-
+    }
+    return houses;
 }
+
+module.exports = seed;
---
+++ seed/index.test.js
@@ -0,0 +1,13 @@
+const seed = require('./index.js');
+
+
+
+test('generates 100 house listings', () => {
+    const houses = seed();
+    expect(houses.length).toBe(100);
+})
+
+test('generates listing ids as zero padded strings', () => {
+    const house = seed()[1];
+    expect(house['Listing_id']).toBe('001');
+})
