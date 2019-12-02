84cbcf58af4cd8c69a497e83f1956713a7a304b1
Kyle Morgan <kmorgan8588@gmail.com>
2019-12-02 15:38:29 -0800

    fixes path error, which was preventing index from rendering bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'public', 'dist'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
