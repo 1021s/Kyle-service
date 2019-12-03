d056d8a2f4db39985f2a90b16bc488d45cbced03
Kyle Morgan <kmorgan8588@gmail.com>
2019-12-02 16:13:17 -0800

    fixed some issues from the last merge

---
+++ client/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            house: {}
+        }
+    }
+    
+
+    render() {
+        return (
+            <div>
+                Hello There
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
