ea0284935bdb37d2a8b40877a03d435119c1cd97
Kyle Morgan <kmorgan8588@gmail.com>
2019-12-12 18:52:25 -0800

    for local, update mongo uri

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
+
 const URI = 'mongodb://localhost:27017/listings';
 const db = mongoose.connect(URI, {useNewUrlParser: true,  useUnifiedTopology: true });
-
-
-
 
 
 module.exports = db;
